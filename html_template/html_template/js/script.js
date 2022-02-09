a2df7cce531e3a5442a9c61dc6ffbc1b43f13d1d
Oihan75 <oihanernaga@gmail.com>
2022-02-09 17:17:28 +0100

    exos html

---
+++ html_template/html_template/js/script.js
@@ -0,0 +1,7 @@
+let texte= prompt("Bonjour","<entrer ici votre prénom>");
+if (reponse==null){
+    alert("vous avez annulé");
+} else {
+    alert ("Bonjour"+réponse+",ça va ?");
+}
+//document.body.innerHTML += '<h1> Coucou! </h1>';
