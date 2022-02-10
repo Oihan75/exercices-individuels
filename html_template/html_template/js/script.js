7ea354fcf70bed9ec48e46042bf37747252fc36e
Oihan75 <oihanernaga@gmail.com>
2022-02-10 11:32:25 +0100

    exos html 2

--- html_template/html_template/js/script.js
+++ html_template/html_template/js/script.js
@@ -1,7 +1,16 @@
-let texte= prompt("Bonjour","<entrer ici votre prénom>");
-if (reponse==null){
-    alert("vous avez annulé");
+let texte= prompt("Bonjour","<entrer ici votre prenom>");
+if (texte==null){
+    alert("vous avez annule");
 } else {
-    alert ("Bonjour"+réponse+",ça va ?");
+document.getElementById("Coucou").innerHTML= "Bonjour "+texte+", bienvenue.";
 }
-//document.body.innerHTML += '<h1> Coucou! </h1>';
+
+let an= prompt("Annee de naissance?","<entrer ici votre annee de naissance>");
+let today= new Date();
+let annee= today.getFullYear();
+if (an==null){
+   alert("vous avez annule");
+} else{
+    let age= annee-an;
+    document.getElementById("age").innerHTML= "Vous avez "+age+" ans.";
+}
