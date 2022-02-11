be9976fe4addabcb0b7fe182d1eab8832c01b2b9
Oihan75 <oihanernaga@gmail.com>
2022-02-11 10:51:05 +0100

    exos js

--- html_template/html_template/js/script.js
+++ html_template/html_template/js/script.js
@@ -1,16 +1,23 @@
+function askName(){
 let texte= prompt("Bonjour","<entrer ici votre prenom>");
 if (texte==null){
     alert("vous avez annule");
 } else {
 document.getElementById("Coucou").innerHTML= "Bonjour "+texte+", bienvenue.";
 }
-
-let an= prompt("Annee de naissance?","<entrer ici votre annee de naissance>");
+}
+function askBirthYear(){
+let an= prompt("Date de naissance?","<entrer ici votre date de naissance (mm/jj/aaaa)>");
 let today= new Date();
-let annee= today.getFullYear();
+let birth= new Date(an);
+console.log(today);
+console.log(birth);
 if (an==null){
    alert("vous avez annule");
 } else{
-    let age= annee-an;
+    let age= Math.trunc((today-birth)/(365.25*24*60*60*1000));
     document.getElementById("age").innerHTML= "Vous avez "+age+" ans.";
 }
+}
+askName();
+askBirthYear();
